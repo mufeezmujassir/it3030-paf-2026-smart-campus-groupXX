053b16c9a60a39b2b7c9b1d026896b4ee05125ef
mufeezmujassir <mufeezmujassir80@gmail.com>
2026-03-05 09:44:48 +0530

    enviroment setup for the frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+  return (
+    <div>
+        <h1 className="text-4xl font-bold underline text-center mt-10 bg-amber-900">
+    Hello world!
+  </h1>
+
+    </div>
+  )
+}
+
+export default App
